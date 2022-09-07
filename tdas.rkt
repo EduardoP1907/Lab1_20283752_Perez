d06629a35feec923a48c3a2620dd7b5f2d97b8d1
EduardoP1907 <eduardo.perez.s@usach.cl>
2022-09-07 01:10:49 -0300

    commit 3, tdas

---
+++ tdas.rkt
@@ -0,0 +1,25 @@
+#lang racket
+
+
+;TDA imagen
+;CONSTRUCTOR
+;Representación: Ancho X Largo X [pixbit-d* |  pixrgb-d* | pixhe*]
+
+(define(imagen ancho largo .pixel)
+  lista(ancho largo .pixel))
+
+;;SELECTORES(GET)
+
+;Selector del elemento "ancho", perteneciente al TDA imagen
+;Dominio; Lista
+;Recorrido; Int
+(define(get-ancho lista) (car lista))
+
+
+
+
+
+;TDA bitmap?
+;Dominio: TDA imagen
+;Recorrido: boleano
+(define (bitmap? 
