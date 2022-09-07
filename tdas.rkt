c798e1e3a3b4ade6fa9e33e470605a98a1807a98
EduardoP1907 <eduardo.perez.s@usach.cl>
2022-09-07 01:53:21 -0300

    commit 4, GET y SET tda imagen

--- tdas.rkt
+++ tdas.rkt
@@ -15,11 +15,30 @@
 ;Recorrido; Int
 (define(get-ancho lista) (car lista))
 
+;Selector del elemento "largo", perteneciente al TDA imagen
+;Dominio; Lista
+;Recorrido; Int
+(define(get-largo lista) (cadr lista))
 
+;Selector del elemento "pixel", perteneciente al TDA imagen
+;Dominio; Lista
+;Recorrido; Int
+(define(get-pixel lista) (caddr lista))
 
+;;MODIFICADORES(SET)
+;Modificaodr del largo de la imagen
+;Dominio: Lista
+;Recorrido: Lista
+(define(set-largo mi-imagen new-largo)
+  (imagen new-largo
+          (get-ancho mi-imagen)
+          (get-pixel mi-imagen)))
 
+;Modificaodr del ancho de la imagen
+;Dominio: Lista
+;Recorrido: Lista
+(define (set-ancho mi-imagen new-ancho)
+  (imagen( get-largo mi-imagen)
+         new-ancho
+         (get-pixel mi-imagen)))
 
-;TDA bitmap?
-;Dominio: TDA imagen
-;Recorrido: boleano
-(define (bitmap? 
