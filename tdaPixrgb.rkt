88b5fe487fe2d93b5bab1040caf41ce03159c5d9
EduardoP1907 <eduardo.perez.s@usach.cl>
2022-09-14 17:02:49 -0300

    commit 9. termino tda pixrgb

--- tdaPixrgb.rkt
+++ tdaPixrgb.rkt
@@ -3,17 +3,57 @@
 ;CONSTRUCTOR
 ;Representación: x (int) X y (int) X r (C) X g (C) X b(C) X d (int)
 
-(define(pixbit-d x y bit depth)
-  list(x y bit depth))
+(define(pixbit-d x y r g b d)
+  list(x y r g b d))
 
 ;;SELECTORES(GET)
 
 ;Selector del elemento "x", perteneciente al TDA pixrgb
 ;Dominio; Lista
 ;Recorrido; Int
-(define(get-x lista) (car lista))
+(define(get-x list) (car list))
 
 ;Selector del elemento "y", perteneciente al TDA pixrgb
 ;Dominio; Lista
 ;Recorrido; Int
-(define(get-y lista) (cadr lista))
+(define(get-y list) (cadr list))
+
+;Selector del elemento "r", perteneciente al TDA pixrgb
+;Dominio; Lista
+;Recorrido; Int
+(define(get-r list) (caddr list))
+
+;Selector del elemento "g", perteneciente al TDA pixrgb
+;Dominio; Lista
+;Recorrido; Int
+(define(get-g list) (cadddr list))
+
+;Selector del elemento "b", perteneciente al TDA pixrgb
+;Dominio; Lista
+;Recorrido; Int
+(define(get-b lista) (caddddr lista))
+
+;Selector del elemento "d", perteneciente al TDA pixrgb
+;Dominio; Lista
+;Recorrido; Int
+(define(get-d lista) (cadddddr lista))
+
+;;MODIFICADORES(SET)
+;Modificador del tda pixrgb-d de la imagen
+;Dominio: Lista
+;Recorrido: int
+
+(define (set-valor pixel valor)
+  (pixrgx-d (get-x pixel)
+            (get-y pixel)
+            (get-r pixel)
+            (get-g pixel)
+            (get-b pixel)
+            valor (get-d pixel)))
+
+
+
+
+
+
+
