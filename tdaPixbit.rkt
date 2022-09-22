518a20ca14e1decdd8e0333a93c355a7ed1e1669
EduardoP1907 <eduardo.perez.s@usach.cl>
2022-09-22 18:31:22 -0300

    commit 12. Creación pixbit?

--- tdaPixbit.rkt
+++ tdaPixbit.rkt
@@ -1,5 +1,5 @@
 #lang racket
-
+(require "FuncionesEscenciales.rkt")
 ;TDA pixbit-d
 ;CONSTRUCTOR
 ;Representación: x (int) X y (int) X bit ([0|1]) X depth (int))
@@ -28,6 +28,19 @@
 ;Dominio; Lista
 ;Recorrido; Int
 (define(get-depth lista) (cadddr lista))
+
+;PERTENENCIA
+(define pixbit?
+  (lambda(pixel)
+    (cond
+      [(empty? pixel)#f]
+      [(not(=(length pixel)2))#f]
+      [(not(=(length(second pixel))2))#f]
+      [(not(number?(first(first pixel))))#f]
+      [(not(number?(second(first pixel))))#f]
+      [(not(or(equal?(first(second pixel))0 equal?(first(second pixel))1)))#f]
+      [(not(number?(second(second pixel))))#f]
+      [else #t])))
 
 ;;MODIFICADORES(SET)
 ;Modificaodr del tda pixbit-d de la imagen
